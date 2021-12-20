0f448c6d626a054cee90406bc709c3dcd78137b8
manoj ganesh <manojblr1203@gmail.com>
2021-12-20 11:53:59 +0000

    first commit

---
+++ paralell.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  
+int main()  
+{  
+  int base=4;  
+  int height=18;  
+  int area_parallelogram=base*height;  
+  printf("Area of the parallelogram=%d",area_parallelogram);  
+  return 0;  
+}  
